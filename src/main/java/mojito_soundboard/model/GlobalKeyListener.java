033fe3e6b4fca19a4ded967ec3292f994088e5df
Typhon <loic.sculier@hotmail.fr>
2018-11-03 00:38:03 +0100

    temp fix for false CTRL jnativehook

--- src/main/java/mojito_soundboard/model/GlobalKeyListener.java
+++ src/main/java/mojito_soundboard/model/GlobalKeyListener.java
@@ -1,30 +1,62 @@
 package mojito_soundboard.model;
 
+import org.jnativehook.GlobalScreen;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Loïc Sculier aka typhon0
  */
 public class GlobalKeyListener implements NativeKeyListener {
 
-    public GlobalKeyListener() {
+    private NativeKeyEvent previousKeyEvent = null;
 
+    public GlobalKeyListener() {
+        // Get the logger for "org.jnativehook" and set the level
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.OFF);
+        logger.setUseParentHandlers(false);
     }
 
     @Override
     public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
-
-
     }
 
     @Override
     public void nativeKeyPressed(NativeKeyEvent nativeKeyEvent) {
+        if (nativeKeyEvent.getKeyCode() == NativeKeyEvent.VC_O && isControlModifierPressed(nativeKeyEvent) && isControlModifierPressed(previousKeyEvent)) {
+        }
+        previousKeyEvent = nativeKeyEvent;
 
     }
 
     @Override
     public void nativeKeyReleased(NativeKeyEvent nativeKeyEvent) {
 
+    }
+
+    /**
+     * Detects if ShiftModifier is pressed
+     *
+     * @param e
+     * @return True if yes , false if not
+     */
+    private boolean isShiftModifierPressed(NativeKeyEvent e) {
+        return e.getModifiers() == NativeKeyEvent.SHIFT_L_MASK || e.getModifiers() == NativeKeyEvent.SHIFT_R_MASK || e.getModifiers() == NativeKeyEvent.SHIFT_MASK
+                || e.getModifiers() == NativeKeyEvent.VC_SHIFT;
+    }
+
+    /**
+     * Detects if ControlModifier is pressed
+     *
+     * @param e
+     * @return True if yes , false if not
+     */
+    private boolean isControlModifierPressed(NativeKeyEvent e) {
+        return (e.getModifiers() == NativeKeyEvent.CTRL_L_MASK || e.getModifiers() == NativeKeyEvent.CTRL_R_MASK || e.getModifiers() == NativeKeyEvent.CTRL_MASK
+                || e.getModifiers() == NativeKeyEvent.VC_CONTROL);
     }
 }
